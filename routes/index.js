82daa2ef217395db8cf366990f64695a6a23c021
Hyunshin-Kim <50730678+Hyunshin-Kim@users.noreply.github.com>
2024-05-17 10:17:02 +0900

    Update index.js
    test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log(req.getAll());
+  //console.log(req.getAll());
   res.render('index.html');
 });
 
